a5b344af277391193a5131d448561e9a4d3e82ff
Benton Li <cl2597@cornell.edu>
2025-01-16 01:23:08 +0000

    Add content

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,26 @@
-import './global.css'
-import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import { Navbar } from './components/nav'
-import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/next'
-import Footer from './components/footer'
-import { baseUrl } from './sitemap'
+import "./global.css";
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import { Navbar } from "./components/nav";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import Footer from "./components/footer";
+import { baseUrl } from "./sitemap";
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: "Benton Li",
+    template: "%s | Benton Li",
   },
-  description: 'This is my portfolio.',
+  description: "Benton's Website",
   openGraph: {
-    title: 'My Portfolio',
-    description: 'This is my portfolio.',
+    title: "Benton Li",
     url: baseUrl,
-    siteName: 'My Portfolio',
-    locale: 'en_US',
-    type: 'website',
+    siteName: "Benton Li",
+    locale: "en_US",
+    type: "website",
   },
   robots: {
     index: true,
@@ -29,29 +28,32 @@ export const metadata: Metadata = {
     googleBot: {
       index: true,
       follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
     },
   },
-}
+};
 
-const cx = (...classes) => classes.filter(Boolean).join(' ')
+const cx = (...classes) => classes.filter(Boolean).join(" ");
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html
       lang="en"
       className={cx(
-        'text-black bg-white dark:text-white dark:bg-black',
+        "text-black bg-white",
         GeistSans.variable,
         GeistMono.variable
       )}
     >
+      <head>
+        <link rel="icon" href="/static/logo.png" sizes="any" />
+      </head>
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
@@ -62,5 +64,5 @@ export default function RootLayout({
         </main>
       </body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,37 @@
-import { BlogPosts } from 'app/components/posts'
+import { BlogPosts } from "app/components/posts";
+const research_int = " ";
+
+const intro = "I'm a quant";
+
+const academic_bg =
+  "I received B.S. and M.Eng in Computer Science from Cornell University, where I was fortunate to be advised by Lorenzo Alvisi and Sainyam Galhotra";
 
 export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Benton Li
       </h1>
-      <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+      <p className="mb-2">
+        I'm a quant developer and database system researcher based in London 🇬🇧
       </p>
-      <div className="my-8">
+      <p className="mb-2">
+        At work, my specializations span from fixed income (rates & credits) to
+        xVA. I also know a thing or two about regulation framework (Basel IV,
+        UMR, SIMM, FRTB, etc)
+      </p>
+      <p className="mb-2">
+        In academia, my research interests mainly lie in causal inference,
+        causal discovery algorithm, and their application in finance.
+      </p>
+      <p className="mb-2">
+        I'm also good at multitasking, in both parallel computing and real life
+        :))
+      </p>
+
+      {/* <div className="my-8">
         <BlogPosts />
-      </div>
+      </div> */}
     </section>
-  )
+  );
 }
